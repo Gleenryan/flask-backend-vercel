bff08bb27762a3c1b919a8ae92ca1a3ff28367e1
Gleen Ryan <99118090+Gleenryan@users.noreply.github.com>
2025-12-24 08:37:05 +0700

    images masuk

--- api/index.py
+++ api/index.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
+import os
 
 app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, "..", "static")
 
 @app.route("/")
 def home():
@@ -9,9 +13,9 @@ def home():
         "message": "Flask backend on Vercel 🔥"
     })
 
-@app.route("/hello")
-def hello():
-    name = request.args.get("name", "Gleen")
-    return jsonify({
-        "hello": name
-    })
+@app.route("/image/<filename>")
+def get_image(filename):
+    return send_from_directory(
+        os.path.join(STATIC_DIR, "images"),
+        filename
+    )
